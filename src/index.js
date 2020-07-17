39aa7f125f63087d8ac19f1147f3301a440344bd
Karthikraj S <karthikrajs@Karthikrajs-MacBook-Pro.local>
2020-07-17 18:21:38 +0530

    index.js routing config

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {Provider} from 'react-redux';
+import decode from 'jwt-decode'
+import thunk from 'redux-thunk';
+import {BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './rootReducer';
+import {userLoggedIn} from "./actions/auth";
+// import * as serviceWorker from './serviceWorker';
+
+const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk))
+);
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+      <Provider store={store}>
+          <Route children={ App } />
+      </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ src/rootReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import user from './reducers/user';
+// import devices from './reduces/devices'
+
+export default combineReducers({
+    user,
+    // devices
+})
